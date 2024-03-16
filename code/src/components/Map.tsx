96d9581393cb174a6018bcaa627a079c0aa1cdd7
ShailenderGoyal <114686807+ShailenderGoyal@users.noreply.github.com>
2024-03-17 04:56:29 +0530

    Added No Kill Zones on Maps

--- code/src/components/Map.tsx
+++ code/src/components/Map.tsx
@@ -1,63 +1,65 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { Paper } from "@mui/material";
-import { Point } from "ol/geom";
 import "ol/ol.css";
-import { RMap, RLayerTileWebGL, RLayerGraticule, RControl, RStyle } from "rlayers";
-import { fromLonLat, getPointResolution } from "ol/proj";
-import { RefObject, useRef } from "react";
+import { RMap, RLayerTileWebGL, RLayerVector, RLayerGraticule, RControl, RStyle, RFeature } from "rlayers";
+import { RView } from "rlayers/RMap";
+import { fromLonLat } from "ol/proj";
+import Point from "ol/geom/Point";
 import { OnlyConnected } from "./Only";
 import { DronesMapManager } from "./DronesMapManager";
 import { FODMapManager } from "./FODMapManager";
 import { DateTimeDisplay } from "./DateTimeDisplay";
 import AlertBar from "./AlertBar";
-import "./Map.css"
-
+import "./Map.css";
 
 const centerPoint = [78.34910677877723, 17.445657887972082] as [number, number];
 const center = fromLonLat(centerPoint);
+const noKillZones = [
+  { latitude: 17.45, longitude: 78.34, radius: 100 },
+  { latitude: 17.46, longitude: 78.35, radius: 500 },
+  // Add more no-kill zones as needed
+];
 
-export default function Simple({
-  toggleTheme,
-}: {
-  toggleTheme: () => void;
-}): JSX.Element {
-  const [graticule, setGraticule] = React.useState<boolean>(true);
+export default function Simple({ toggleTheme }: { toggleTheme: () => void }): JSX.Element {
+  const [graticule, setGraticule] = useState<boolean>(true);
   const style = RStyle.useRStyle();
-
-  const mapRef = useRef() as RefObject<RMap>;
-
-  // Here in calculating resolution, 1 depicts 1km width, so that the map shown on the screen will be showing 1km width
-  const resolution = getPointResolution("EPSG:3857", 1, center);
-  const initialZoom = Math.round(Math.log2(156543.03392 / resolution));
+  const mapRef = useRef<RMap>(null);
+  const [view, setView] = React.useState<RView>({ center: center, zoom: 11 });
 
   const toggleGraticule = useCallback(() => {
     setGraticule((prevGraticule) => !prevGraticule);
   }, []);
 
+  const calculateCircleRadius = (radius: number, resolution: number) => {
+    return radius / resolution;
+  };
+
   return (
-    <Paper className="mapWrapper" style={{ position: "relative", height: "100%" }}>
+    <Paper className="mapWrapper" style={{ position: "relative" }}>
       <RStyle.RStyle ref={style}>
-        <RStyle.RStroke
-          color={graticule ? "black" : "transparent"}
-          width={1.5}
-        />
+        <RStyle.RStroke color={graticule ? "black" : "transparent"} width={1.5} />
       </RStyle.RStyle>
       <RMap
         width={"100%"}
         height={"100%"}
-        initial={{ center, zoom: initialZoom }}
+        initial={view} view={[view, setView]}
         ref={mapRef}
-        onClick={useCallback(() => {}, [])}>
+        onClick={useCallback(() => {}, [])}
+      >
         <RLayerTileWebGL url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}" />
+        <RLayerVector zIndex={10}>
+          {noKillZones.map((zone, index) => (
+            <RFeature key={index} geometry={new Point(fromLonLat([zone.longitude, zone.latitude]))}>
+              <RStyle.RStyle>
+                <RStyle.RCircle radius={calculateCircleRadius(zone.radius, view.resolution)}>
+                  <RStyle.RFill color="red" />
+                </RStyle.RCircle>
+              </RStyle.RStyle>
+            </RFeature>
+          ))}
+        </RLayerVector>
+
         <RLayerGraticule visible={graticule} strokeStyle={style} />
-        {/* <RLayerVector zIndex={100}>
-          <RStyle.RStyle>
-            <RStyle.RCircle radius={1}>
-              <RStyle.RFill color="red" />
-            </RStyle.RCircle>
-            <RFeature geometry={new Point(center)} />
-          </RStyle.RStyle>
-        </RLayerVector> */}
         <RControl.RScaleLine />
         <RControl.RZoomSlider />
         <RControl.RRotate autoHide={false} />
@@ -71,7 +73,7 @@ export default function Simple({
         </OnlyConnected>
         <FODMapManager />
       </RMap>
-      <AlertBar toggleGraticule={toggleGraticule} toggleTheme={toggleTheme}/>
+      <AlertBar graticule={graticule} toggleGraticule={toggleGraticule} toggleTheme={toggleTheme} />
     </Paper>
   );
 }
