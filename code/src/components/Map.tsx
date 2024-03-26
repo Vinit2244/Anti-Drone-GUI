e7b0fa3b3449522900d9765012f0c54e41424402
Shailender Goyal <shailendergoyal@pop-os.localdomain>
2024-03-26 15:20:51 +0530

    NokillZones adding from gui to map

--- code/src/components/Map.tsx
+++ code/src/components/Map.tsx
@@ -1,6 +1,10 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Paper } from "@mui/material";
+import {
+  NoKillZone
+} from "../types/payloads";
 import "ol/ol.css";
+import { invoke } from "@tauri-apps/api";
 import {
   RMap,
   RLayerTileWebGL,
@@ -9,7 +13,6 @@ import {
   RControl,
   RStyle,
   RFeature,
-  ROverlay,
 } from "rlayers";
 import { RView } from "rlayers/RMap";
 import { fromLonLat, getPointResolution } from "ol/proj";
@@ -20,33 +23,45 @@ import { FODMapManager } from "./FODMapManager";
 import { DateTimeDisplay } from "./DateTimeDisplay";
 import AlertBar from "./AlertBar";
 import "./Map.css";
-import ActionMenu from "./ActionMenu"
+import ActionMenu from "./ActionMenu";
 import { FODDataProvider } from "../contexts/FODDataContext";
 import { PhaseProvider } from "../contexts/PhaseContext";
 
-
 const centerPoint = [149.1615057, -35.3606176] as [number, number]; // [Longitude, Latitude]
 const center = fromLonLat(centerPoint);
-const noKillZones = [
-  { latitude: 17.45, longitude: 78.34, radius: 100 }, // Radius in Meters
-  { latitude: 17.46, longitude: 78.35, radius: 500 },
-  // Add more no-kill zones as needed
-];
 
 export default function Simple({
-  toggleTheme, cName, toggleFullScreenMap 
+  toggleTheme,
+  cName,
+  toggleFullScreenMap 
 }: {
   toggleTheme: () => void;
   cName: string;
-  toggleFullScreenMap: () => void}): JSX.Element {
+  toggleFullScreenMap: () => void;
+}): JSX.Element {
   const [graticule, setGraticule] = useState<boolean>(true);
-  const [otherComponentClass, setOtherComponentClass] = React.useState("");
   const style = RStyle.useRStyle();
-  const mapRef = useRef<RMap>(null);
-  // Here in calculating resolution, 1 depicts 1km width, so that the map shown on the screen will be showing 1km width
+
+  const [noKillZones, setNoKillZones] = useState<NoKillZone[]>([]);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    fetchNoKillZones();
+  }, []);
+
+  const fetchNoKillZones = async () => {
+    try {
+      console.log("fetching")
+      const newNoKillZones = (await invoke("get_no_kill_zones")) as NoKillZone[];
+      setNoKillZones(newNoKillZones);
+    } catch (error) {
+      console.error("Error fetching no-kill zones:", error);
+    }
+  };
+
   const resolution = getPointResolution("EPSG:3857", 1, center);
   const initialZoom = Math.round(Math.log2(156543.03392 / resolution));
-  const [view, setView] = React.useState<RView>({
+  const [view, setView] = useState<RView>({
     center: center,
     zoom: initialZoom,
   });
@@ -60,14 +75,9 @@ export default function Simple({
   };
 
   return (
-   
     <Paper className={cName} style={{ position: "relative" }}>
- 
       <RStyle.RStyle ref={style}>
-        <RStyle.RStroke
-          color={graticule ? "black" : "transparent"}
-          width={1.5}
-        />
+        <RStyle.RStroke color={graticule ? "black" : "transparent"} width={1.5} />
       </RStyle.RStyle>
       <RMap
         width={"100%"}
@@ -75,19 +85,20 @@ export default function Simple({
         initial={view}
         view={[view, setView]}
         ref={mapRef}
-        onClick={useCallback(() => { }, [])}>
+        onClick={useCallback(() => { }, [])}
+      >
         <RLayerTileWebGL url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}" />
         <RLayerVector zIndex={10}>
           {noKillZones.map((zone, index) => (
             <RFeature
               key={index}
-              geometry={new Point(fromLonLat([zone.longitude, zone.latitude]))}>
+              geometry={new Point(fromLonLat([zone.longitude, zone.latitude]))}
+            >
               <RStyle.RStyle>
+
                 <RStyle.RCircle
-                  radius={calculateCircleRadius(
-                    zone.radius,
-                    view?.resolution || 0
-                  )}>
+                  radius={calculateCircleRadius(zone.radius, view?.resolution || 0)}
+                >
                   <RStyle.RStroke color="#8B0000" width={2} />
                   <RStyle.RFill color="rgba(255, 153, 153, 0.5)" />
                 </RStyle.RCircle>
@@ -109,11 +120,10 @@ export default function Simple({
         </OnlyConnected>
         <FODMapManager />
       </RMap>
-      
       <AlertBar
         toggleGraticule={toggleGraticule}
         toggleTheme={toggleTheme}
-        toggleFullScreenMap = {toggleFullScreenMap}
+        toggleFullScreenMap={toggleFullScreenMap}
       />
     </Paper>
   );
