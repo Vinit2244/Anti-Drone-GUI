2908b8fd9ee78aa72e8d73c556ea7953e2b061fd
Vinit Mehta <vinitmehta@Vinits-MacBook-Pro.local>
2024-03-31 18:13:22 +0530

    coloration of ready button states

--- code/src/components/SwipeableReadyDrawer.tsx
+++ code/src/components/SwipeableReadyDrawer.tsx
@@ -49,48 +49,119 @@ export default function SwipeableReadyDrawer() {
 
                 setState({ ...state, [anchor]: open });
             };
+
+    // States of all the ready options
+    const [readyButtonStates, setReadyButtonStates] = React.useState({
+        'Pre-Arm Check': 'warning',
+        'Gyro': 'error',
+        'Accelerometer': 'success',
+        'Magnetometer': 'warning',
+        'Absolute Pressure': 'warning',
+        'GPS': 'warning',
+        'Angular Rate Control': 'warning',
+        'Altitude Stabilization': 'warning',
+        'Yaw Position': 'warning',
+        'Z Altitude Control': 'warning',
+        'X/Y Position Control': 'warning',
+        'Motor Outputs/Control': 'warning',
+        'RC Receiver': 'warning',
+        'AHRS': 'warning',
+        'Terrain': 'warning',
+        'Battery': 'warning',
+        'Propulsion': 'warning',
+        'GeoFence': 'warning',
+        'Logging': 'warning',
+        'Proximity': 'warning'
+    });
+
+    
+
     const iconList = [
-        <DoneIcon />, // For 'Pre-Arm Check'
-        <img src={GyroscopeIcon} height={24} />, // For 'Gyro'
-        <img src={AccelerometerIcon} height={24} />, // For 'Accelerometer'
-        <ExploreIcon />, // For 'Magnetometer'
-        <CompressIcon />, // For 'Absolute Pressure'
-        <ShareLocationIcon />, // For 'GPS'
-        <CropRotateIcon />, // For 'Angular Rate Control'
-        <img src={AltitudeStabilizationIcon} height={24} />, // For 'Altitude Stabilization'
-        <img src={YawIcon} height={24} />, // For 'Yaw Position'
-        <HeightIcon />, // For 'Z Altitude Control'
-        <img src={XYIcon} height={24} />, // For 'X/Y Position Control'
-        <img src={MotorIcon} height={24} />, // For 'Motor Outputs/Control'
-        <SettingsRemoteIcon />, // For 'RC Receiver'
-        <img src={XYZAxisIcon} height={24} />, // For 'AHRS'
-        <TerrainIcon />, // For 'Terrain'
-        <BatteryChargingFullIcon />, // For 'Battery'
-        <RocketLaunchIcon />, // For 'Propulsion'
-        <FenceIcon />, // For 'GeoFence'
-        <WysiwygIcon />, // For 'Logging'
-        <img src={ProximityIcon} height={24} />, // For 'Proximity'
+        <DoneIcon />,                                           // For 'Pre-Arm Check'
+        <img src={GyroscopeIcon} height={24} />,                // For 'Gyro'
+        <img src={AccelerometerIcon} height={24} />,            // For 'Accelerometer'
+        <ExploreIcon />,                                        // For 'Magnetometer'
+        <CompressIcon />,                                       // For 'Absolute Pressure'
+        <ShareLocationIcon />,                                  // For 'GPS'
+        <CropRotateIcon />,                                     // For 'Angular Rate Control'
+        <img src={AltitudeStabilizationIcon} height={24} />,    // For 'Altitude Stabilization'
+        <img src={YawIcon} height={24} />,                      // For 'Yaw Position'
+        <HeightIcon />,                                         // For 'Z Altitude Control'
+        <img src={XYIcon} height={24} />,                       // For 'X/Y Position Control'
+        <img src={MotorIcon} height={24} />,                    // For 'Motor Outputs/Control'
+        <SettingsRemoteIcon />,                                 // For 'RC Receiver'
+        <img src={XYZAxisIcon} height={24} />,                  // For 'AHRS'
+        <TerrainIcon />,                                        // For 'Terrain'
+        <BatteryChargingFullIcon />,                            // For 'Battery'
+        <RocketLaunchIcon />,                                   // For 'Propulsion'
+        <FenceIcon />,                                          // For 'GeoFence'
+        <WysiwygIcon />,                                        // For 'Logging'
+        <img src={ProximityIcon} height={24} />,                // For 'Proximity'
     ];
+
+    const getButtonStyle = (state: string) => {
+        let baseStyle;
+        switch (state) {
+            case 'warning':
+                baseStyle = { border: '1px solid #ffa500', borderRadius: '8px', margin: '2px', backgroundColor: 'rgba(255, 165, 0, 0.15)' };
+                break;
+            case 'success':
+                baseStyle = { border: '1px solid #008001', borderRadius: '8px', margin: '2px', backgroundColor: 'rgba(0, 128, 1, 0.15)' };
+                break;
+            case 'error':
+                baseStyle = { border: '1px solid #ff0000', borderRadius: '8px', margin: '2px', backgroundColor: 'rgba(255, 0, 0, 0.15)' };
+                break;
+            default:
+                baseStyle = { border: '1px solid #ffffff', borderRadius: '8px', margin: '2px', backgroundColor: 'rgba(255, 0, 0, 0.15)' };
+        }
+    
+        let hoverStyle;
+        switch (state) {
+            case 'warning':
+                hoverStyle = {
+                    '&:hover': {
+                        backgroundColor: 'rgba(255, 165, 0, 0.3)',
+                    }
+                };
+                break;
+            case 'success':
+                hoverStyle = {
+                    '&:hover': {
+                        backgroundColor: 'rgba(0, 128, 1, 0.3)',
+                    }
+                };
+                break;
+            case 'error':
+                hoverStyle = {
+                    '&:hover': {
+                        backgroundColor: 'rgba(255, 0, 0, 0.3)',
+                    }
+                };
+                break;
+            default:
+                hoverStyle = {};
+        }
+    
+        return { ...baseStyle, ...hoverStyle };
+    };
 
     const list = (anchor: Anchor) => (
         <Box
             sx={{ width: 250 }}
             role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
         >
-            <List>
-                {['Pre-Arm Check', 'Gyro', 'Accelerometer', 'Magnetometer', 'Absolute Pressure', 'GPS', 'Angular Rate Control', 'Altitude Stabilization', 'Yaw Position', 'Z Altitude Control', 'X/Y Position Control', 'Motor Outputs/Control', 'RC Receiver', 'AHRS', 'Terrain', 'Battery', 'Propulsion', 'GeoFence', 'Logging', 'Proximity'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {iconList[index]}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
+         <List sx={{backgroundColor: "black"}}>
+            {Object.entries(readyButtonStates).map(([component, state], index) => (
+                <ListItem key={component} disablePadding>
+                    <ListItemButton sx={getButtonStyle(state)} disableTouchRipple>
+                        <ListItemIcon>
+                            {iconList[index]}
+                        </ListItemIcon>
+                        <ListItemText primary={component} />
+                    </ListItemButton>
+                </ListItem>
+            ))}
+        </List>
         </Box>
     );
 
