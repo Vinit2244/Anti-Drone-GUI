f72c1e516900552e53911669e7ec8c6fccbe5dbf
Prithvi <79378970+Prithvi-k@users.noreply.github.com>
2024-04-07 11:31:02 +0530

    Fix comment in IsEnemyDrone

--- code/src/components/IsEnemyDrone.tsx
+++ code/src/components/IsEnemyDrone.tsx
@@ -3,10 +3,10 @@ export function IsEnemyDrone(drone_id: number) {
   //   The function takes in the drone id
   //   Returns 0 if the drone is friendly and 1 if the drone is an enemy
   if (drone_id % 2 === 1) {
-    //   If the drone id is even, it is a friendly drone
+    //   If the drone id is odd, it is a friendly drone
     return 0;
   } else {
-    // If the drone id is odd, it is an enemy drone
+    // If the drone id is even, it is an enemy drone
     return 1;
   }
 }
