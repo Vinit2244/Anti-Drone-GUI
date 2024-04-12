9fd784a7136d3addee95ff0e092140d90ab6b84f
Vinit Mehta <vinitmehta@Vinits-MacBook-Pro.local>
2024-04-12 20:14:59 +0530

    added the friendly drone check in ready button

--- code/src/components/IsEnemyDrone.tsx
+++ code/src/components/IsEnemyDrone.tsx
@@ -2,7 +2,7 @@ export function IsEnemyDrone(drone_id: number) {
   //   Function to select differentiate between friendly and enemy drones
   //   The function takes in the drone id
   //   Returns 0 if the drone is friendly and 1 if the drone is an enemy
-  if (drone_id % 2 === 1) {
+  if (drone_id % 2 === 0) {
     //   If the drone id is odd, it is a friendly drone
     return 0;
   } else {
