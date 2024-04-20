ec577b156e0197ad7fe6e63fb7f654a46b771cec
Manasa Kalaimalai <53469845+manasakalaimalai@users.noreply.github.com>
2024-04-20 14:35:23 +0530

    fixed slide to confirm on kill

--- code/src/components/SliderButton.tsx
+++ code/src/components/SliderButton.tsx
@@ -113,25 +113,91 @@ export function SliderButton_Kill({
   threshold?: number;
   disabled?: boolean;
 }) {
-  const [value, setValue] = useState(0);
-
+  const [value, setValue] = useState(10);
+  const sliderRef = useRef(value);
   const handleSliderChange = (event: Event, newValue: number | number[]) => {
-    setValue(newValue as number);
-    if (newValue === 100) {
-      onSubmit?.(); // Invoke onSubmit callback when slider is slid completely
-    }
+    const newVal = Math.max(Math.min(newValue as number, 90), 10);
+    setValue(newVal);
+    sliderRef.current = newVal;
   };
 
   return (
-    <div style={{ width: "10%", marginTop: "5px", padding: "0 10px" }}>
-      <Typography gutterBottom>{trackText}</Typography>
-      <Slider
-        disabled={disabled}
-        value={value}
-        onChange={handleSliderChange}
-        sx={{ width: "100%" }}
-      />
-      <Typography gutterBottom>{railText}</Typography>
-    </div>
+    <Slider
+      disabled={disabled}
+      slotProps={{
+        thumb: {
+          onMouseDown: (e) => {
+            document.addEventListener(
+              "mouseup",
+              () => {
+                const normalisedVal = ((sliderRef.current - 10) * 100) / 80;
+                if (normalisedVal > threshold) onSubmit?.();
+                setValue(() => {
+                  sliderRef.current = 10;
+                  return 10;
+                });
+              },
+              { once: true }
+            );
+          },
+          style: {
+            width: "5%",
+            borderRadius: "16px",
+            height: "35px",
+          },
+        },
+        track: {
+          style: {
+            // width: "200px",
+            height: "35px",
+            borderRadius: "16px",
+          },
+          children: (
+            <Typography
+              style={{
+                height: "100%",
+                color: "white",
+                textAlign: "center",
+                display: "flex",
+                fontSize: "12px",
+                fontWeight: "bold",
+                justifyContent: "center",
+                alignItems: "center",
+                overflow: "hidden",
+                whiteSpace: "nowrap",
+              }}
+            >
+              {trackText}
+            </Typography>
+          ),
+        },
+        rail: {
+          style: {
+            width: "95%",
+            height: "40px",
+            borderRadius: "16px",
+          },
+          children: (
+            <Typography
+              style={{
+                height: "100%",
+                color: "black",
+                // fontWeight: "bold",
+                fontSize: "11px",
+                textAlign: "center",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {railText}
+            </Typography>
+          ),
+        },
+      }}
+      value={value}
+      onChange={handleSliderChange}
+      sx={{ width: "100%", marginTop: "15px", marginBottom: "15px" }}
+    />
   );
 }
